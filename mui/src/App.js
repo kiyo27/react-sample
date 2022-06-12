0300cd03a68e9307d2c35dc34e08511d328216e2
kiyo27 <19531773+kiyo27@users.noreply.github.com>
2022-06-12 20:46:17 +0900

    add mui dashboard

---
+++ mui/src/App.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import DashboardContent from './components/Dashboard';
+
+function App() {
+  return <DashboardContent />
+}
+
+export default App;
